128fb2c8bf9b633171abbdffce91ed6cd76de810
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-08 16:53:15 +0530

    ⚰

--- userbot/utils.py
+++ userbot/utils.py
@@ -8,6 +8,7 @@ from userbot import CMD_LIST
 import re
 import logging
 import inspect
+import time
 
 def command(**args):
     args["func"] = lambda e: e.via_bot_id is None
@@ -20,7 +21,7 @@ def command(**args):
         return print("stupidity at its best")
     else:
         pattern = args.get("pattern", None)
-        allow_sudo = args.get("allow_sudo", None)
+        allow_sudo = args.get("allow_sudo", False)
         allow_edited_updates = args.get('allow_edited_updates', False)
         args["incoming"] = args.get("incoming", False)
         args["outgoing"] = True
@@ -50,7 +51,7 @@ def command(**args):
                 pass
 
         if allow_sudo:
-            args["from_users"] = list(Var.SUDO_USERS)
+            args["from_users"] = list(Config.SUDO_USERS)
             # Mutually exclusive with outgoing (can only set one of either).
             args["incoming"] = True
         del allow_sudo
@@ -291,7 +292,7 @@ def errors_handler(func):
 
     return wrapper
 
-async def progress(current, total, event, start, type_of_ps, file_name=None):
+async def progress(current, total, event, start, type_of_ps):
     """Generic progress_callback for both
     upload.py and download.py"""
     now = time.time()
@@ -302,9 +303,9 @@ async def progress(current, total, event, start, type_of_ps, file_name=None):
         elapsed_time = round(diff) * 1000
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
-        progress_str = "[{0}{1}]\nProgress: {2}%\n".format(
-            ''.join(["█" for i in range(math.floor(percentage / 5))]),
-            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
+        progress_str = "[{0}{1}]\nPercent: {2}%\n".format(
+            ''.join(["█" for _ in range(math.floor(percentage / 5))]),
+            ''.join(["░" for _ in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
         tmp = progress_str + \
             "{0} of {1}\nETA: {2}".format(
@@ -312,12 +313,10 @@ async def progress(current, total, event, start, type_of_ps, file_name=None):
                 humanbytes(total),
                 time_formatter(estimated_total_time)
             )
-        if file_name:
-            await event.edit("{}\nFile Name: `{}`\n{}".format(
-                type_of_ps, file_name, tmp))
-        else:
-            await event.edit("{}\n{}".format(type_of_ps, tmp))
-
+        await event.edit("{}\n {}".format(
+            type_of_ps,
+            tmp
+        ))
 
 def humanbytes(size):
     """Input size in bytes,
