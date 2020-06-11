61a952cdbb9cf1d5ae1b38a65367d54e1c26ebbb
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-11 09:37:16 +0530

    ❌

--- userbot/plugins/Bloom.py
+++ userbot/plugins/Bloom.py
@@ -58,7 +58,7 @@ async def autopic(event):
         fnt = ImageFont.truetype(FONT_FILE_TO_USE, 60)
         ofnt = ImageFont.truetype(FONT_FILE_TO_USE, 250)
         drawn_text.text((200, 400), current_time, font=fnt, fill=(FR,FG,FB))
-        drawn_text.text((250, 250), "      😎", font = ofnt, fill=(FR,FG,FB))
+        drawn_text.text((250, 250), "😎", font = ofnt, fill=(FR,FG,FB))
         img.save(photo)
         file = await event.client.upload_file(photo)  # pylint:disable=E0602
         try:
