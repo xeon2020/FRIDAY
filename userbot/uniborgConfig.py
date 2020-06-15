f4f487ef4e674d55c583cb65f5c479b3c62a5475
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-15 19:06:35 +0530

    ❌

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -64,6 +64,8 @@ if ENV:
         NC_LOG_P_M_S = bool(os.environ.get("NC_LOG_P_M_S", True))
         
         # send .get_id in any channel to forward all your NEW PMs to this group
+        PRIVATE_GROUP_BOT_API_ID = os.environ.get("PRIVATE_GROUP_BOT_API_ID",None)
+        # send .get_id in your private channel to forward all your Private messages
         PM_LOGGR_BOT_API_ID = os.environ.get("PM_LOGGR_BOT_API_ID", None)
         if PM_LOGGR_BOT_API_ID:
             PM_LOGGR_BOT_API_ID = int(PM_LOGGR_BOT_API_ID)
