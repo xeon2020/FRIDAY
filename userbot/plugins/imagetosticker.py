31e7a6fb135c8f6b870f15086e98f16bdaa54445
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-20 22:11:33 +0530

    Ok hahahah

--- userbot/plugins/imagetosticker.py
+++ userbot/plugins/imagetosticker.py
@@ -1,5 +1,5 @@
 """
-QuotLy: Avaible commands: .sbot
+QuotLy: Avaible commands: .imagetosticker
 """
 import datetime
 from telethon import events
@@ -7,12 +7,12 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd(pattern="itos ?(.*)"))
+@borg.on(admin_cmd(pattern="imagetosticker ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user image message.```")
+       await event.edit("```Reply to any user message.```")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.media:
@@ -37,36 +37,3 @@ async def _(event):
           else: 
              await event.delete()
              await event.client.send_message(event.chat_id, response.message)
-
-            
-@borg.on(admin_cmd(pattern="stoi ?(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return 
-    if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user sticker message.```")
-       return
-    reply_message = await event.get_reply_message() 
-    if not reply_message.media:
-       await event.edit("```Reply to sticker message```")
-       return
-    chat = "@StickerToPicbot"
-    sender = reply_message.sender
-    if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
-       return
-    await event.edit("```Making a image```")
-    async with event.client.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1020956520))
-              await event.client.forward_messages(chat, reply_message)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("``` unblock me (@StickerToPicbot) to work```")
-              return
-          if response.text.startswith("Hi!"):
-             await event.edit("```Can you kindly disable your forward privacy settings for good?```")
-          else: 
-             await event.delete()
-             await event.client.send_message(event.chat_id, response.message)
-            
