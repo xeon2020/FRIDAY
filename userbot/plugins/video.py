49d36aeb840a98f1a1a65cd5fe893954be358782
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-21 22:17:01 +0530

    ❌

---
+++ userbot/plugins/video.py
@@ -0,0 +1,39 @@
+"""
+Avaible commands: .vid
+"""
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd(pattern="vid ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("```Reply to any user message.```")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.media:
+       await event.edit("```Reply to YouTube link```")
+       return
+    chat = "@DownloadStickersBot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("```Reply to actual users message.```")
+       return
+    await event.edit("```uploading```")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=599312585))
+              await event.client.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Please unblock me (@VideoTubeBot) ```")
+              return
+          if response.text.startswith("Hi!"):
+             await event.edit("```Can you kindly disable your forward privacy settings for good?```")
+          else: 
+             await event.delete()
+             await event.client.send_message(event.chat_id, response.message)
