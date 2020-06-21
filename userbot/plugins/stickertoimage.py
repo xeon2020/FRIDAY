f40ec1a3d97842b675e5140bb9b9a5661ff7bf99
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-21 21:39:44 +0530

    Ufffff 🤫

--- userbot/plugins/stickertoimage.py
+++ userbot/plugins/stickertoimage.py
@@ -26,7 +26,7 @@ async def _(event):
     await event.edit("```Making a sticker to image```")
     async with event.client.conversation(chat) as conv:
           try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=164977173))
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=658563529))
               await event.client.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
