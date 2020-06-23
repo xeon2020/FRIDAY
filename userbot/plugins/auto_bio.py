a2a9aca3f8da33c571dd47b70b34828bc84a928e
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-23 19:13:03 +0530

    Ok our gang 😛

--- userbot/plugins/auto_bio.py
+++ userbot/plugins/auto_bio.py
@@ -23,7 +23,7 @@ async def _(event):
     while True:
         DMY = time.strftime("%d.%m.%Y")
         HM = time.strftime("%H:%M:%S")
-        bio = f"📅 {DMY} |  👨‍💻  | ⌚️ {HM}"
+        bio = f"📅 {DMY} |#CyberProGang| ⌚️ {HM}"
         logger.info(bio)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
