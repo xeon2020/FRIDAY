624a043f71dfcf9f0234f0ee8f5453ad6489499a
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-24 14:19:57 +0530

    Ok

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -100,7 +100,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [custom.Button.inline(
-        "{} {} {}".format ("📌", x , "📌"),
+        "{} {} {}".format ("🔰", x , "🔰"),
         data="us_plugin_{}".format(x))
         for x in helpable_plugins]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
@@ -111,7 +111,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
     if len(pairs) > number_of_rows:
         pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
             [
-            (custom.Button.inline("Previous⏪", data="{}_prev({})".format(prefix, modulo_page)),
-             custom.Button.inline("Next⏩", data="{}_next({})".format(prefix, modulo_page)))
+            (custom.Button.inline("◀️Previous", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline("Next▶️", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
