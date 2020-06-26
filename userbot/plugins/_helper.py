946b88efd1d4d2cf18350e3b2f8ace44f34dee4b
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-26 07:49:37 +0530

    Ok

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -1,18 +1,22 @@
 from userbot import CMD_LIST
+from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
+from platform import uname
 import sys
 from telethon import events, functions, __version__
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "leobrownlee"
 
 @command(pattern="^.help ?(.*)")
 #@borg.on(admin_cmd(pattern=r"help ?(.*)"))
 async def cmd_list(event):
-    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!", "-", "_"):
         tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
         input_str = event.pattern_match.group(1)
         if tgbotusername is None or input_str == "text":
             string = ""
             for i in CMD_LIST:
-                string += "📌 " + i + "\n"
+                string += "⚡️" + i + "\n"
                 for iter_list in CMD_LIST[i]:
                     string += "    `" + str(iter_list) + "`"
                     string += "\n"
@@ -41,7 +45,8 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Userbot Helper.. \nProvided by [FRIDAY](https://github.com/leobrownlee/FRIDAY)\n`Userbot Helper to reveal all the commands`"""
+            help_string = f"""Userbot Helper.. Provided by ✨{DEFAULTUSER}✨ \n
+`Userbot Helper to reveal all the commands`\n__Do .help plugin_name for commands, in case popup doesn't appear.__"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
@@ -52,19 +57,41 @@ async def cmd_list(event):
                 hide_via=True
             )
             await event.delete()
+            
+@borg.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    result = await borg(functions.help.GetNearestDcRequest())  # pylint:disable=E0602
+    await event.edit(result.stringify())
+
+
+@borg.on(admin_cmd(pattern="config"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    result = await borg(functions.help.GetConfigRequest())  # pylint:disable=E0602
+    result = result.stringify()
+    logger.info(result)  # pylint:disable=E0602
+    await event.edit("""Telethon UserBot powered by FRIDAY""")
+
 
 @borg.on(admin_cmd(pattern="syntax (.*)"))
 async def _(event):
     if event.fwd_from:
         return
     plugin_name = event.pattern_match.group(1)
-    if plugin_name in borg._plugins:
-        help_string = borg._plugins[plugin_name].__doc__
-        unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © @UniBorg"
+
+    if plugin_name in CMD_LIST:
+        help_string = CMD_LIST[plugin_name].__doc__
+        unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © FRIDAY"
+        
         if help_string:
             plugin_syntax = f"Syntax for plugin **{plugin_name}**:\n\n{help_string}\n{unload_string}"
         else:
             plugin_syntax = f"No DOCSTRING has been setup for {plugin_name} plugin."
     else:
-        plugin_syntax = "Enter valid **Plugin** name.\nDo `.exec ls stdplugins` or `.helpme` to get list of valid plugin names."
+
+        plugin_syntax = "Enter valid **Plugin** name.\nDo `.plinfo` or `.help` to get list of valid plugin names."
+
     await event.edit(plugin_syntax)
