43ac23b1cea11baad8f23dfae42e5d8a0bcbdcf8
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-26 09:42:13 +0530

    Ok

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -214,7 +214,7 @@ from userbot.utils import admin_cmd
 import io
 import userbot.plugins.sql_helper.pmpermit_sql as pmpermit_sql
 from telethon import events
-@bot.on(events.NewMessage(incoming=True, from_users=(1080421146)))
+@bot.on(events.NewMessage(incoming=True, from_users=()))
 async def hehehe(event):
     if event.fwd_from:
         return
