16fcad0e7488f341630f51466eaa416441db94ac
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-28 11:44:09 +0530

    🤪❌

--- userbot/plugins/wallpapers.py
+++ userbot/plugins/wallpapers.py
@@ -3,11 +3,19 @@
 import requests , re , random 
 import urllib , os 
 from telethon.tl import functions
+from userbot import ALIVE_NAME
 from datetime import datetime
 from PIL import Image, ImageDraw, ImageFont
 from userbot.utils import admin_cmd
 import asyncio
 from time import sleep
+
+FONT_FILE_TO_USE = "Fonts/1942.ttf"
+
+
+
+DEFAULTUSER = str(ALIVE_NAME)
+
 
 AUTOPFP_PACK = os.environ.get("AUTOPFP_PACK", None)
 if AUTOPFP_PACK is None:
@@ -59,7 +67,66 @@ async def animepp():
 @borg.on(admin_cmd(pattern="wallpapers ?(.*)"))
 async def main(event):
     await event.edit("**uploading wallpapers \ndone check ur DP.**") 
+    downloaded_file_name = "./FRIDAY/original_pic.png"
+
+    downloader = SmartDL(Var.DOWNLOAD_PFP_URL_CLOCK, downloaded_file_name, progress_bar=False)
+
+    downloader.start(blocking=False)
+
+    photo = "photo_pfp.png"
+
+    while not downloader.isFinished():
+
+        place_holder = None
     while True:
+
+#RIP Danger zone Here no editing here plox
+
+        R = random.randint(0,256)
+
+        B = random.randint(0,256)
+
+        G = random.randint(0,256)
+
+        FR = (256 - R) 
+
+        FB = (256 - B) 
+
+        FG = (256 - G) 
+
+        shutil.copy(downloaded_file_name, photo)
+
+        image = Image.open(photo)
+
+        image.paste( (R, G, B), [0,0,image.size[0],image.size[1]])
+
+        image.save(photo)
+
+        
+
+        #Edit only Below part ð Or esle u will be responsible ð¤·ââ
+
+        current_time = datetime.now().strftime("\n\n Time: %H:%M:%S \n \n Date: %d/%m/%y")
+
+        img = Image.open(photo)
+
+        drawn_text = ImageDraw.Draw(img)
+
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 40)
+
+        ofnt = ImageFont.truetype(FONT_FILE_TO_USE, 40)
+
+        drawn_text.text((200, 400), current_time, font=fnt, fill=(FR,FG,FB))
+
+        drawn_text.text((250, 250), f"{DEFAULTUSER}", font = ofnt, fill=(0,FG,0))
+
+        img.save(photo)
+
+        file = await event.client.upload_file(photo)  # pylint:disable=E0602
+
+
+
+
       try:
         await animepp()
         file = await event.client.upload_file("donottouch.jpg")
