25cbbd7213901cab9de9b07bdf76505cfd164b3a
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-06-30 16:53:39 +0530

    Ok

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -1,3 +1,9 @@
+# special thanks to Sur_vivor 
+# first on friday by leobrownlee 
+
+
+
+
 from telethon import events
 from datetime import datetime
 from userbot.__init__ import StartTime
@@ -8,8 +14,8 @@ async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    await event.edit("Pong!")
+    await event.edit("🏓 Pong!")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     uptime = get_readable_time((time.time() - StartTime))
-    await event.edit(f"PONG!\nPong: {ms}\nUptime: {uptime}")
+    await event.edit(f"🏓PONG!\nPong speed: {ms}\n🤖Userbot Uptime: {uptime}")
