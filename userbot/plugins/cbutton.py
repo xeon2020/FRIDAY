f017f77b26e6e8c1d19237c7d49174d31bbc415d
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-07-01 15:47:44 +0530

    Okay

--- userbot/plugins/cbutton.py
+++ userbot/plugins/cbutton.py
@@ -3,7 +3,7 @@
 
 import re
 from telethon import custom
-from uniborg.util import admin_cmd
+from userbot.util import admin_cmd
 
 
 # regex obtained from: https://github.com/PaulSonOfLars/tgbot/blob/master/tg_bot/modules/helper_funcs/string_handling.py#L23
@@ -39,7 +39,7 @@ async def _(event):
 
         # if even, not escaped -> create button
         if n_escapes % 2 == 0:
-            # create a thruple with button label, url, and newline status
+            # create a tuple with button label, url, and newline status
             buttons.append((match.group(2), match.group(3), bool(match.group(4))))
             note_data += markdown_note[prev:match.start(1)]
             prev = match.end(1)
@@ -48,7 +48,7 @@ async def _(event):
         else:
             note_data += markdown_note[prev:to_check]
             prev = match.start(1) - 1
-        
+    else:
         note_data += markdown_note[prev:]
 
     message_text = note_data.strip()
