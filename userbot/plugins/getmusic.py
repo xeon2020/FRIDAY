c766d9cd9662dcbd984816c4b7807ec78526adb3
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-07-01 08:38:34 +0530

    ❌

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -36,11 +36,11 @@ async def _(event):
     await event.edit("Ok finding Your Awesome Song From My Database ◉‿◉")    
     bruh(str(cmd))
     l = glob.glob("*.mp3")
-    loa = l[]
+    
     await event.edit("Sending Song From My Database To Telegram")
     await borg.send_file(
                 event.chat_id,
-                loa,
+                
                 force_document=True,
                 allow_cache=False,
                 caption=cmd,
