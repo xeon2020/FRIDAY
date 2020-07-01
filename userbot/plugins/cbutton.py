0577d6ed1d3c2dd39b4ca774817324a47f40cb51
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-07-01 15:48:27 +0530

    Okay

--- userbot/plugins/cbutton.py
+++ userbot/plugins/cbutton.py
@@ -3,7 +3,7 @@
 
 import re
 from telethon import custom
-from userbot.util import admin_cmd
+from userbot.utils import admin_cmd
 
 
 # regex obtained from: https://github.com/PaulSonOfLars/tgbot/blob/master/tg_bot/modules/helper_funcs/string_handling.py#L23
